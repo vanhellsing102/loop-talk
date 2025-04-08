730c11aadeb1323120de3a9121ffdd2226cbd8ff
murad <vvanhellsing30@gmail.com>
2025-04-08 20:43:52 +0600

    user model created

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nuqw3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(res =>{
+    console.log("mongoose connect");
+})
+.catch(error =>{
+    console.log(error);
+})
 
 app.get('/', async(req, res) =>{
     res.send("Loop Talk server is running");
---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["male", "female"]
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    }
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
