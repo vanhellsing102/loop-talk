719ebff69db73b73db93ef2a1d313ba1c67b3e71
murad <vvanhellsing30@gmail.com>
2025-04-13 14:50:34 +0600

    logout functionality

--- frontend/src/component/Sidebar/LogOut.jsx
+++ frontend/src/component/Sidebar/LogOut.jsx
@@ -1,8 +1,29 @@
 import { CiLogout } from "react-icons/ci";
+import UseAixosPublic from "../../hooks/useAixosPublic";
+import { useAuthContext } from "../../ContextProviders/AuthContextProvider";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const LogOut = () => {
+    const axiosPublic = UseAixosPublic();
+    const {setAuthUser} = useAuthContext();
+    const navigate = useNavigate();
+
+    const handleLogOut = () =>{
+        // console.log("logout");
+        axiosPublic.get("/auth/logout")
+        .then(res =>{
+            console.log(res.data);
+            if(res.data){
+                localStorage.removeItem("user");
+                setAuthUser(null);
+                navigate("/signin");
+                toast.success(res.data.message);
+            }
+        })
+    }
     return (
-        <button className="mt-7 cursor-pointer">
+        <button onClick={handleLogOut} className="mt-7 cursor-pointer">
             <CiLogout className="text-2xl text-red-500"></CiLogout>
         </button>
     );
