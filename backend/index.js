05807b10cb95ebe8d4baadb4dc487ab633f28963
murad <vvanhellsing30@gmail.com>
2025-04-29 11:21:03 +0600

    frontend add

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://loop-talk-frontent.vercel.app"],
     credentials: true
 }));
 
